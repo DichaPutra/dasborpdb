462624f3f8b5edbd334d6fb49831ac9fb9dbbfcd
dichaputra <arwindra23@gmail.com>
2021-02-22 22:28:12 +0700

    Revert "add migration"
    This reverts commit 8e1ef52c52ce8a32b556fcb171496b0d5f08a491.

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,3 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
